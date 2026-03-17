8e64dd9e1b11812342b01ef22927d46b000f1188
Alanis Venerruche de Carvalho <alanis.carvalho@aluno.senai.br>
2026-03-17 09:52:28 -0300

    Documentação com Swagger + Insomnia + Entrega do Bloco 1

--- src/app.js
+++ src/app.js
@@ -1,24 +1,28 @@
 // src/app.js
 const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./swagger");
 const app = express();
-// Middleware para ler JSON no body
+// Middlewares
 app.use(express.json());
-// Importar rotas
+// Documentação Swagger
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+// Rotas
 const eventoRoutes = require("./routes/eventoRoutes");
 const participanteRoutes = require("./routes/participanteRoutes");
 const inscricaoRoutes = require("./routes/inscricaoRoutes");
-app.use("/inscricoes", inscricaoRoutes);
-app.use("/participantes", participanteRoutes);
-// Usar rotas com prefixo
 app.use("/eventos", eventoRoutes);
-// Rota raiz (informativa)
+app.use("/participantes", participanteRoutes);
+app.use("/inscricoes", inscricaoRoutes);
+// Rota raiz
 app.get("/", (req, res) => {
     res.json({
         mensagem: "API de Notificações",
+        documentacao: "/api-docs",
         rotas: {
             eventos: "/eventos",
             participantes: "/participantes",
-            inscricoes: '/inscricoes',
+            inscricoes: "/inscricoes",
         },
     });
 });
